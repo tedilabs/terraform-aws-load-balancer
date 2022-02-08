7e288e42bbeef0e1252c9476e3143d85e09a5a78
Byungjin Park (Claud) <posquit0.bj@gmail.com>
2022-02-09 03:39:00 +0900

    Add nlb module (#3)

---
+++ modules/nlb/main.tf
@@ -0,0 +1,97 @@
+locals {
+  metadata = {
+    package = "terraform-aws-load-balancer"
+    version = trimspace(file("${path.module}/../../VERSION"))
+    module  = basename(path.module)
+    name    = var.name
+  }
+  module_tags = var.module_tags_enabled ? {
+    "module.terraform.io/package"   = local.metadata.package
+    "module.terraform.io/version"   = local.metadata.version
+    "module.terraform.io/name"      = local.metadata.module
+    "module.terraform.io/full-name" = "${local.metadata.package}/${local.metadata.module}"
+    "module.terraform.io/instance"  = local.metadata.name
+  } : {}
+}
+
+
+data "aws_availability_zones" "available" {
+  state = "available"
+}
+
+data "aws_subnet" "this" {
+  for_each = var.network_mapping
+
+  id = each.value.subnet_id
+}
+
+locals {
+  load_balancer_type              = "NETWORK"
+  available_availability_zone_ids = data.aws_availability_zones.available.zone_ids
+  network_mapping = {
+    for zone_id in local.available_availability_zone_ids :
+    zone_id => try(merge({
+      cidr_block           = data.aws_subnet.this[zone_id].cidr_block != "" ? data.aws_subnet.this[zone_id].cidr_block : null
+      ipv6_cidr_block      = data.aws_subnet.this[zone_id].ipv6_cidr_block != "" ? data.aws_subnet.this[zone_id].ipv6_cidr_block : null
+      private_ipv4_address = null
+      ipv6_address         = null
+      allocation_id        = null
+    }, var.network_mapping[zone_id]), null)
+  }
+  enabled_network_mapping = {
+    for zone_id, config in local.network_mapping :
+    zone_id => config
+    if try(config.subnet_id, null) != null
+  }
+  availability_zone_ids = keys(local.enabled_network_mapping)
+}
+
+# INFO: Not supported attributes
+# - `customer_owned_ipv4_pool`
+# - `desync_mitigation_mode`
+# - `drop_invalid_header_fields`
+# - `enable_http2`
+# - `enable_waf_fail_open`
+# - `idle_timeout`
+# - `security_groups`
+resource "aws_lb" "this" {
+  name = var.name
+
+  load_balancer_type = lower(local.load_balancer_type)
+  internal           = !var.is_public
+  ip_address_type    = lower(var.ip_address_type)
+
+  dynamic "subnet_mapping" {
+    for_each = local.enabled_network_mapping
+
+    content {
+      subnet_id = subnet_mapping.value.subnet_id
+
+      private_ipv4_address = try(subnet_mapping.value.private_ipv4_address, null)
+      ipv6_address         = try(subnet_mapping.value.ipv6_address, null)
+      allocation_id        = try(subnet_mapping.value.allocation_id, null)
+    }
+  }
+
+  dynamic "access_logs" {
+    for_each = var.access_log_enabled ? ["go"] : []
+
+    content {
+      enabled = var.access_log_enabled
+      bucket  = var.access_log_s3_bucket
+      prefix  = var.access_log_s3_key_prefix
+    }
+  }
+
+  ## Attributes
+  enable_cross_zone_load_balancing = var.cross_zone_load_balancing_enabled
+  enable_deletion_protection       = var.deletion_protection_enabled
+
+  tags = merge(
+    {
+      "Name" = local.metadata.name
+    },
+    local.module_tags,
+    var.tags,
+  )
+}
---
+++ modules/nlb/variables.tf
@@ -0,0 +1,103 @@
+variable "name" {
+  description = "(Required) The name of the load balancer. This name must be unique within your AWS account, can have a maximum of 32 characters, must contain only alphanumeric characters or hyphens, and must not begin or end with a hyphen."
+  type        = string
+
+  validation {
+    condition     = length(var.name) <= 32
+    error_message = "The name can have a maximum of 32 characters, must contain only alphanumeric characters or hyphens, and must not begin or end with a hyphen."
+  }
+}
+
+variable "is_public" {
+  description = "(Optional) Indicates whether the load balancer will be public. Defaults to `false`."
+  type        = bool
+  default     = false
+}
+
+variable "ip_address_type" {
+  description = "(Optional) The type of IP addresses used by the subnets for your load balancer. The possible values are `IPV4` and `DUALSTACK`."
+  type        = string
+  default     = "IPV4"
+
+  validation {
+    condition     = contains(["IPV4", "DUALSTACK"], var.ip_address_type)
+    error_message = "The possible values are `IPV4` and `DUALSTACK`."
+  }
+}
+
+variable "network_mapping" {
+  description = <<EOF
+  (Optional) The configuration for the load balancer how routes traffic to targets in which subnets, and in accordance with IP address settings. Select at least one Availability Zone and one subnet for each zone. We recommend selecting at least two Availability Zones. The load balancer will route traffic only to targets in the selected Availability Zones. Zones that are not supported by the load balancer or VPC cannot be selected. Subnets can be added, but not removed, once a load balancer is created. Each key of `network_mapping` is the availability zone id like `apne2-az1`, `use1-az1`. Each value of `network_mapping` block as defined below.
+    (Required) `subnet_id` - The id of the subnet of which to attach to the load balancer. You can specify only one subnet per Availability Zone.
+    (Optional) `private_ipv4_address` - A private ipv4 address within the subnet to assign to the internal load balancer.
+    (Optional) `ipv6_address` - An ipv6 address within the subnet to assign to the internet-facing load balancer.
+    (Optional) `allocation_id` - The allocation ID of the Elastic IP address.
+  EOF
+  type        = map(map(string))
+  default     = {}
+}
+
+variable "access_log_enabled" {
+  description = "(Optional) Indicates whether to enable access logs. Defaults to `false`, even when bucket is specified."
+  type        = bool
+  default     = false
+}
+
+variable "access_log_s3_bucket" {
+  description = "The name of the S3 bucket used to store the access logs."
+  type        = string
+  default     = null
+}
+
+variable "access_log_s3_key_prefix" {
+  description = "The key prefix for the specified S3 bucket."
+  type        = string
+  default     = null
+}
+
+variable "cross_zone_load_balancing_enabled" {
+  description = "(Optional) Cross-zone load balancing distributes traffic evenly across all targets in the Availability Zones enabled for the load balancer. Indicates whether to enable cross-zone load balancing. Defaults to `false`. Regional data transfer charges may apply when cross-zone load balancing is enabled."
+  type        = bool
+  default     = false
+}
+
+variable "deletion_protection_enabled" {
+  description = "(Optional) Indicates whether deletion of the load balancer via the AWS API will be protected. Defaults to `false`."
+  type        = bool
+  default     = false
+}
+
+variable "tags" {
+  description = "(Optional) A map of tags to add to all resources."
+  type        = map(string)
+  default     = {}
+}
+
+variable "module_tags_enabled" {
+  description = "(Optional) Whether to create AWS Resource Tags for the module informations."
+  type        = bool
+  default     = true
+}
+
+
+###################################################
+# Resource Group
+###################################################
+
+variable "resource_group_enabled" {
+  description = "(Optional) Whether to create Resource Group to find and group AWS resources which are created by this module."
+  type        = bool
+  default     = true
+}
+
+variable "resource_group_name" {
+  description = "(Optional) The name of Resource Group. A Resource Group name can have a maximum of 127 characters, including letters, numbers, hyphens, dots, and underscores. The name cannot start with `AWS` or `aws`."
+  type        = string
+  default     = ""
+}
+
+variable "resource_group_description" {
+  description = "(Optional) The description of Resource Group."
+  type        = string
+  default     = "Managed by Terraform."
+}
