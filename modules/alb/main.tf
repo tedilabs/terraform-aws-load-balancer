637b96da978c865bf891060066df9e7c16aff8ef
Byungjin Park (Claud) <posquit0.bj@gmail.com>
2022-02-11 02:29:52 +0900

    Add alb module (#6)

---
+++ modules/alb/main.tf
@@ -0,0 +1,90 @@
+locals {
+  metadata = {
+    package = "terraform-aws-load-balancer"
+    version = trimspace(file("${path.module}/../../VERSION"))
+    module  = basename(path.module)
+    name    = var.name
+  }
+  module_tags = var.module_tags_enabled ? {
+    "module.terraform.io/package"   = local.metadata.package
+    "module.terraform.io/version"   = local.metadata.version
+    "module.terraform.io/name"      = local.metadata.module
+    "module.terraform.io/full-name" = "${local.metadata.package}/${local.metadata.module}"
+    "module.terraform.io/instance"  = local.metadata.name
+  } : {}
+}
+
+
+data "aws_availability_zones" "available" {
+  state = "available"
+}
+
+data "aws_subnet" "this" {
+  for_each = var.network_mapping
+
+  id = each.value.subnet_id
+}
+
+locals {
+  load_balancer_type = "APPLICATION"
+
+  available_availability_zone_ids = data.aws_availability_zones.available.zone_ids
+  network_mapping = {
+    for zone_id in local.available_availability_zone_ids :
+    zone_id => try(merge({
+      cidr_block = data.aws_subnet.this[zone_id].cidr_block != "" ? data.aws_subnet.this[zone_id].cidr_block : null
+    }, var.network_mapping[zone_id]), null)
+  }
+  enabled_network_mapping = {
+    for zone_id, config in local.network_mapping :
+    zone_id => config
+    if try(config.subnet_id, null) != null
+  }
+  availability_zone_ids = keys(local.enabled_network_mapping)
+}
+
+# INFO: Not supported attributes
+# - `enable_cross_zone_load_balancing`
+# - `customer_owned_ipv4_pool`
+resource "aws_lb" "this" {
+  name = var.name
+
+  load_balancer_type = lower(local.load_balancer_type)
+  internal           = !var.is_public
+  ip_address_type    = lower(var.ip_address_type)
+  security_groups    = var.security_groups
+
+  dynamic "subnet_mapping" {
+    for_each = local.enabled_network_mapping
+
+    content {
+      subnet_id = subnet_mapping.value.subnet_id
+    }
+  }
+
+  dynamic "access_logs" {
+    for_each = var.access_log_enabled ? ["go"] : []
+
+    content {
+      enabled = var.access_log_enabled
+      bucket  = var.access_log_s3_bucket
+      prefix  = var.access_log_s3_key_prefix
+    }
+  }
+
+  ## Attributes
+  desync_mitigation_mode     = lower(var.desync_mitigation_mode)
+  drop_invalid_header_fields = var.drop_invalid_header_fields
+  enable_deletion_protection = var.deletion_protection_enabled
+  enable_http2               = var.http2_enabled
+  enable_waf_fail_open       = var.waf_fail_open_enabled
+  idle_timeout               = var.idle_timeout
+
+  tags = merge(
+    {
+      "Name" = local.metadata.name
+    },
+    local.module_tags,
+    var.tags,
+  )
+}
