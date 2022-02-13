0c2422180ab9ea662b64587ede9ba9a217b79c6c
Byungjin Park (Claud) <posquit0.bj@gmail.com>
2022-02-14 04:19:38 +0900

    Add nlb-listener module (#7)
    * Add nlb-listener module
    
    * Add labels for GitHub

---
+++ modules/nlb-listener/variables.tf
@@ -0,0 +1,89 @@
+variable "load_balancer" {
+  description = "(Required) The ARN of the network load balancer to add the listener."
+  type        = string
+}
+
+variable "port" {
+  description = "(Required) The number of port on which the listener of load balancer is listening."
+  type        = number
+}
+
+variable "protocol" {
+  description = "(Required) The protocol for connections from clients to the load balancer. Valid values are `TCP`, `TLS`, `UDP` and `TCP_UDP`. Not valid to use `UDP` or `TCP_UDP` if dual-stack mode is enabled on the load balancer."
+  type        = string
+
+  validation {
+    condition     = contains(["TCP", "TLS", "UDP", "TCP_UDP"], var.protocol)
+    error_message = "Valid values are `TCP`, `TLS`, `UDP` and `TCP_UDP`. Not valid to use `UDP` or `TCP_UDP` if dual-stack mode is enabled on the load balancer."
+  }
+}
+
+variable "target_group" {
+  description = "(Required) The ARN of the target group to which to route traffic."
+  type        = string
+}
+
+variable "tls_certificate" {
+  description = "(Optional) The ARN of the default SSL server certificate. For adding additional SSL certificates, see the `tls_additional_certificates` variable. Required if `protocol` is `TLS`."
+  type        = string
+  default     = null
+}
+
+variable "tls_additional_certificates" {
+  description = "(Optional) A set of ARNs of the certificate to attach to the listener. This is for additional certificates and does not replace the default certificate on the listener."
+  type        = set(string)
+  default     = []
+}
+
+# INFO:://docs.aws.amazon.com/elasticloadbalancing/latest/network/create-tls-listener.html#describe-ssl-policies
+variable "tls_security_policy" {
+  description = "(Optional) The name of security policy for a Secure Socket Layer (SSL) negotiation configuration. This is used to negotiate SSL connections with clients. Required if protocol is `TLS`. Recommend using the `ELBSecurityPolicy-TLS13-1-2-2021-06` security policy. This security policy includes TLS 1.3, which is optimized for security and performance, and is backward compatible with TLS 1.2."
+  type        = string
+  default     = "ELBSecurityPolicy-TLS13-1-2-2021-06"
+}
+
+variable "tls_alpn_policy" {
+  description = "(Optional) The policy of the Application-Layer Protocol Negotiation (ALPN) to select. ALPN is a TLS extension that includes the protocol negotiation within the exchange of hello messages. Can be set if `protocol` is `TLS`. Valid values are `HTTP1Only`, `HTTP2Only`, `HTTP2Optional`, `HTTP2Preferred`, and `None`. Defaults to `None`."
+  type        = string
+  default     = "None"
+
+  validation {
+    condition     = contains(["None", "HTTP1Only", "HTTP2Only", "HTTP2Optional", "HTTP2Preferred"], var.tls_alpn_policy)
+    error_message = "Valid values are `HTTP1Only`, `HTTP2Only`, `HTTP2Optional`, `HTTP2Preferred`, and `None`. Defaults to `None`."
+  }
+}
+
+variable "tags" {
+  description = "(Optional) A map of tags to add to all resources."
+  type        = map(string)
+  default     = {}
+}
+
+variable "module_tags_enabled" {
+  description = "(Optional) Whether to create AWS Resource Tags for the module informations."
+  type        = bool
+  default     = true
+}
+
+
+###################################################
+# Resource Group
+###################################################
+
+variable "resource_group_enabled" {
+  description = "(Optional) Whether to create Resource Group to find and group AWS resources which are created by this module."
+  type        = bool
+  default     = true
+}
+
+variable "resource_group_name" {
+  description = "(Optional) The name of Resource Group. A Resource Group name can have a maximum of 127 characters, including letters, numbers, hyphens, dots, and underscores. The name cannot start with `AWS` or `aws`."
+  type        = string
+  default     = ""
+}
+
+variable "resource_group_description" {
+  description = "(Optional) The description of Resource Group."
+  type        = string
+  default     = "Managed by Terraform."
+}
