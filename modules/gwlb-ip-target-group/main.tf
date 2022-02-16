71d53e11f6e9ed1e331a82d0273b6a54294a49ab
Byungjin Park <posquit0.bj@gmail.com>
2022-02-16 18:09:29 +0900

    Add `gwlb-ip-target-group` module

---
+++ modules/gwlb-ip-target-group/main.tf
@@ -0,0 +1,114 @@
+locals {
+  metadata = {
+    package = "terraform-aws-load-balancer"
+    version = trimspace(file("${path.module}/../../VERSION"))
+    module  = basename(path.module)
+    name    = var.name
+  }
+  module_tags = var.module_tags_enabled ? {
+    "module.terraform.io/package"   = local.metadata.package
+    "module.terraform.io/version"   = local.metadata.version
+    "module.terraform.io/name"      = local.metadata.module
+    "module.terraform.io/full-name" = "${local.metadata.package}/${local.metadata.module}"
+    "module.terraform.io/instance"  = local.metadata.name
+  } : {}
+}
+
+data "aws_vpc" "this" {
+  id = var.vpc_id
+}
+
+locals {
+  ipv4_regex = "^(\\d+).(\\d+).(\\d+).(\\d+)$"
+
+  ipv4_vpc_cidrs = data.aws_vpc.this.cidr_block_associations.*.cidr_block
+  ipv6_vpc_cidrs = [data.aws_vpc.this.ipv6_cidr_block]
+
+  port = 6081
+  targets = [
+    for target in var.targets : {
+      ip_address = target.ip_address
+      port       = local.port
+      az = anytrue([
+        for cidr in(length(regexall(local.ipv4_regex, target.ip_address)) > 0 ? local.ipv4_vpc_cidrs : local.ipv6_vpc_cidrs) :
+        cidr == cidrsubnet(format("%s/%s", target.ip_address, split("/", cidr)[1]), 0, 0)
+      ]) ? null : "all"
+    }
+  ]
+}
+
+# INFO: Not supported attributes
+# - `connection_termination`
+# - `lambda_multi_value_headers_enabled`
+# - `load_balancing_algorithm_type`
+# - `preserve_client_ip`
+# - `protocol_version`
+# - `proxy_protocol_v2`
+# - `slow_start`
+# - `stickiness`
+# - `tags`
+resource "aws_lb_target_group" "this" {
+  name = var.name
+
+  vpc_id = var.vpc_id
+
+  target_type = "ip"
+  protocol    = "GENEVE"
+  port        = local.port
+
+  ## Attributes
+  deregistration_delay = var.deregistration_delay
+
+  ## INFO: Too many bugs in terraform provider
+  # - `healthy_threshold` doesn't need to be same with `unhealthy_threashold`
+  # - `interval` doesn't apply to the acutal resource
+  # - `timeout` is not supported
+  dynamic "health_check" {
+    for_each = [merge(
+      var.health_check,
+      try(var.health_check.protocol, "TCP") != "TCP"
+      ? {
+        success_codes = "200-399"
+      }
+      : {
+        interval      = null
+        timeout       = null
+        success_codes = null
+        path          = null
+      },
+    )]
+
+    content {
+      enabled = true
+
+      port     = try(health_check.value.port, local.port)
+      protocol = try(health_check.value.protocol, "TCP")
+
+      healthy_threshold   = try(health_check.value.healthy_threshold, 3)
+      unhealthy_threshold = try(health_check.value.unhealthy_threshold, 3)
+      interval            = try(health_check.value.interval, 30)
+      timeout             = try(health_check.value.timeout, 10)
+
+      matcher = health_check.value.success_codes
+      path    = try(health_check.value.path, "/")
+    }
+  }
+}
+
+
+###################################################
+# Attachment for GWLB IP Target Group
+###################################################
+
+resource "aws_lb_target_group_attachment" "this" {
+  for_each = {
+    for target in local.targets :
+    target.ip_address => target
+  }
+
+  target_group_arn = aws_lb_target_group.this.arn
+
+  target_id         = each.value.ip_address
+  port              = each.value.port
+  availability_zone = each.value.az
+}
---
+++ modules/gwlb-ip-target-group/outputs.tf
@@ -0,0 +1,73 @@
+output "arn" {
+  description = "The Amazon Resource Name (ARN) of the target group."
+  value       = aws_lb_target_group.this.arn
+}
+
+output "arn_suffix" {
+  description = "The ARN suffix for use with CloudWatch Metrics."
+  value       = aws_lb_target_group.this.arn_suffix
+}
+
+output "id" {
+  description = "The ID of the target group."
+  value       = aws_lb_target_group.this.id
+}
+
+output "name" {
+  description = "The name of the target group."
+  value       = aws_lb_target_group.this.name
+}
+
+output "vpc_id" {
+  description = "The ID of the VPC which the target group belongs to."
+  value       = aws_lb_target_group.this.vpc_id
+}
+
+output "type" {
+  description = "The target type of the target group."
+  value       = upper(aws_lb_target_group.this.target_type)
+}
+
+output "protocol" {
+  description = "The protocl to use to connect with the target."
+  value       = aws_lb_target_group.this.protocol
+}
+
+output "port" {
+  description = "The port number on which the target receive trrafic."
+  value       = aws_lb_target_group.this.port
+}
+
+output "targets" {
+  description = "A set of targets in the target group."
+  value = [
+    for target in aws_lb_target_group_attachment.this : {
+      ip_address  = target.target_id
+      port        = target.port
+      is_external = target.availability_zone == "all"
+    }
+  ]
+}
+
+output "attributes" {
+  description = "Target Group Attributes that applied to the gateway load balancer."
+  value = {
+    deregistration_delay = aws_lb_target_group.this.deregistration_delay
+  }
+}
+
+output "health_check" {
+  description = "Health Check configuration of the target group."
+  value = {
+    protocol = aws_lb_target_group.this.health_check[0].protocol
+    port     = aws_lb_target_group.this.health_check[0].port
+
+    healthy_threshold   = aws_lb_target_group.this.health_check[0].healthy_threshold
+    unhealthy_threshold = aws_lb_target_group.this.health_check[0].unhealthy_threshold
+    interval            = aws_lb_target_group.this.health_check[0].interval
+    timeout             = aws_lb_target_group.this.health_check[0].timeout
+
+    success_codes = aws_lb_target_group.this.health_check[0].matcher
+    path          = aws_lb_target_group.this.health_check[0].path
+  }
+}
