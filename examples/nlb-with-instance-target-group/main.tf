f1ef4d679526dc9d414ea3c9be7f005d85500d81
Byungjin Park (Claud) <posquit0.bj@gmail.com>
2022-02-18 03:10:08 +0900

    Add examples for nlb (#23)

--- examples/nlb-with-instance-target-group/main.tf
+++ examples/nlb-with-instance-target-group/main.tf
@@ -16,14 +16,17 @@ data "aws_subnet" "default" {
 
 
 ###################################################
-# Gateway Load Balancer
+# Network Load Balancer
 ###################################################
 
-module "gwlb" {
-  source  = "tedilabs/load-balancer/aws//modules/gwlb"
+module "nlb" {
+  source  = "tedilabs/load-balancer/aws//modules/nlb"
   version = "~> 0.2.0"
 
-  name = "tedilabs-gwlb-ip"
+  name = "tedilabs-nlb-instance"
+
+  is_public       = false
+  ip_address_type = "IPV4"
   network_mapping = {
     for az, subnet in data.aws_subnet.default :
     az => {
@@ -36,9 +39,14 @@ module "gwlb" {
   deletion_protection_enabled       = false
 
   listeners = [{
-    port         = 6081
+    port         = 80
+    protocol     = "TCP"
     target_group = module.target_group.arn
   }]
+
+  access_log_enabled       = false
+  access_log_s3_bucket     = "my-bucket"
+  access_log_s3_key_prefix = "/tedilabs-nlb-instance/"
 
   tags = {
     "project" = "terraform-aws-load-balancer-examples"
@@ -47,21 +55,30 @@ module "gwlb" {
 
 
 ###################################################
-# IP Target Group for Gateway Load Balancer
+# Instance Target Group for Network Load Balancer
 ###################################################
 
 module "target_group" {
-  source  = "tedilabs/load-balancer/aws//modules/gwlb-ip-target-group"
+  source  = "tedilabs/load-balancer/aws//modules/nlb-instance-target-group"
   version = "~> 0.2.0"
 
-  name = "tedilabs-gwlb-ip-tg"
+  name = "tedilabs-nlb-instance-tg"
 
   vpc_id = data.aws_vpc.default.id
 
+  port     = 80
+  protocol = "TCP"
+
+  ## Attributes
+  terminate_connection_on_deregistration = false
+  deregistration_delay                   = 300
+  preserve_client_ip                     = true
+  proxy_protocol_v2                      = false
+
   targets = [
-    {
-      ip_address = "10.234.123.8"
-    },
+    # {
+    #   instance = "i-xxxx"
+    # },
   ]
 
   health_check = {
--- examples/nlb-with-ip-target-group/main.tf
+++ examples/nlb-with-ip-target-group/main.tf
@@ -16,14 +16,17 @@ data "aws_subnet" "default" {
 
 
 ###################################################
-# Gateway Load Balancer
+# Network Load Balancer
 ###################################################
 
-module "gwlb" {
-  source  = "tedilabs/load-balancer/aws//modules/gwlb"
+module "nlb" {
+  source  = "tedilabs/load-balancer/aws//modules/nlb"
   version = "~> 0.2.0"
 
-  name = "tedilabs-gwlb-ip"
+  name = "tedilabs-nlb-ip"
+
+  is_public       = false
+  ip_address_type = "IPV4"
   network_mapping = {
     for az, subnet in data.aws_subnet.default :
     az => {
@@ -36,9 +39,14 @@ module "gwlb" {
   deletion_protection_enabled       = false
 
   listeners = [{
-    port         = 6081
+    port         = 80
+    protocol     = "TCP"
     target_group = module.target_group.arn
   }]
+
+  access_log_enabled       = false
+  access_log_s3_bucket     = "my-bucket"
+  access_log_s3_key_prefix = "/tedilabs-nlb-ip/"
 
   tags = {
     "project" = "terraform-aws-load-balancer-examples"
@@ -47,20 +55,33 @@ module "gwlb" {
 
 
 ###################################################
-# IP Target Group for Gateway Load Balancer
+# IP Target Group for Network Load Balancer
 ###################################################
 
 module "target_group" {
-  source  = "tedilabs/load-balancer/aws//modules/gwlb-ip-target-group"
+  source  = "tedilabs/load-balancer/aws//modules/nlb-ip-target-group"
   version = "~> 0.2.0"
 
-  name = "tedilabs-gwlb-ip-tg"
+  name = "tedilabs-nlb-ip-tg"
 
   vpc_id = data.aws_vpc.default.id
 
+  port     = 80
+  protocol = "TCP"
+
+  ## Attributes
+  terminate_connection_on_deregistration = false
+  deregistration_delay                   = 300
+  preserve_client_ip                     = true
+  proxy_protocol_v2                      = false
+
   targets = [
     {
-      ip_address = "10.234.123.8"
+      ip_address = "10.123.123.234"
+    },
+    {
+      ip_address = "10.0.103.34"
+      port       = 999
     },
   ]
 
